b91e19dfeddc77f6353e50c1a2f7e8356465ac54
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-07-14 16:24:54 +0530

    11: Agents Form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,17 +1,49 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-
+import {
+  
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
+import { CarTaxiFrontIcon } from "lucide-react";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
+  // TODO: Change getMany to use protectedProcedure
+
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+
+      return existingAgent;
+    }),
+
+  getMany: protectedProcedure.query(async () => {
     const data = await db.select().from(agents);
 
     // await new Promise((resolve) => setTimeout(resolve, 5000));
 
     // throw new TRPCError({code: "BAD_GATEWAY"})
 
-
     return data;
   }),
+
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+      return createdAgent;
+    }),
 });
