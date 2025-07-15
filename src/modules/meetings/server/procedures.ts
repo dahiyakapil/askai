f184517ee0bd8e9a88921d717a9fd14fd4443259
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-07-15 21:33:53 +0530

    18: Meetings Data Table

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,9 +1,9 @@
 import { db } from "@/db";
-import { meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 import { z } from "zod";
-import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import {
   DEFAULT_PAGE,
   DEFAULT_PAGE_SIZE,
@@ -26,7 +26,7 @@ export const meetingsRouter = createTRPCRouter({
         .returning();
 
       if (!updatedMeeting) {
-        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+        throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found" });
       }
 
       return updatedMeeting;
@@ -87,8 +87,12 @@ export const meetingsRouter = createTRPCRouter({
           // TODO: Change to actual meeting count
 
           ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
@@ -102,6 +106,7 @@ export const meetingsRouter = createTRPCRouter({
       const [total] = await db
         .select({ count: count() })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,25 +1,32 @@
-"use client"
+"use client";
 
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
-import { useTRPC } from "@/trpc/client"
+import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const MeetingsView = () => {
-    const trpc = useTRPC();
-    const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
+  return (
+    <>
+      <div className="flex-1 pb-4 md:px-8 flex flex-col gap-y-4">
+        <DataTable data={data.items} columns={columns} />
 
-    return (
-        <>
-            <div className="overflow-x-scroll">
-                {/* {JSON.stringify(data)} */}
-            </div>
-        </>
-    )
-}
-
-
+        {data.items.length === 0 && (
+          <EmptyState
+            title="Create your first meeting"
+            description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants in real time"
+          />
+        )}
+      </div>
+    </>
+  );
+};
 
 export const MeetingsViewLoading = () => {
   return (
